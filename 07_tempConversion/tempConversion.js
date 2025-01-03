9b691ee8bf7657bf9e6a65abff71d04e6f7ead75
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2025-01-03 16:24:28 -0300

    Create tempConversion logic

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  // x °F ≘ (x − 32) × ⁠5 / 9⁠ °C
+  return +((fahrenheit - 32) *  5 / 9).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // x °C ≘ (x × ⁠9 / 5⁠ + 32) °F
+  return +(celsius * 9 / 5+ 32).toFixed(1);
 };
 
 // Do not edit below this line

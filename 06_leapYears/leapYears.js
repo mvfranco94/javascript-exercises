042ce1488899905f7ee9f993f28c72b42fb86abf
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2025-01-03 16:03:54 -0300

    Add logic to leapYearFunction

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  const isYearDivisibleByFourHundred = year % 400 === 0;
+  const isYearDivisibleByFour = year % 4 === 0;
+  const isNotCentury = year % 100 != 0;
+  return (isNotCentury && isYearDivisibleByFour) || isYearDivisibleByFourHundred  ;
 };
 
 // Do not edit below this line

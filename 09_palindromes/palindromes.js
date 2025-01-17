1214e0d6a305b7b8f548e8feecc677c9768ef45f
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2025-01-17 19:52:22 -0300

    Build palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  const cleanText = text
+    .toLowerCase()
+    .replaceAll(/[^a-zA-Z0-9]/g, '');
+  return  cleanText === cleanText.split('').reverse().join('');
 };
 
 // Do not edit below this line

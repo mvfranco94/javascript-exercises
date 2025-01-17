52453acca660a4fb83cacbe4a003865a733dbedc
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2025-01-17 19:19:45 -0300

    Build calculator functions
    Add logic to the calculator functions and
    use pass all the automated tests.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((product, num) => product * num, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let fact = 1;
+  for (let i = x; i > 0; i--) {
+    fact *= i;
+  }
+
+  return fact;
 };
 
 // Do not edit below this line

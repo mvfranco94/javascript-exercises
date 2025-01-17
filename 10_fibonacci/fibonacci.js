0cd18cbd78d76635deb3bb9d6ef58e73987e8172
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2025-01-17 20:08:04 -0300

    Build fibonacci logic

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(nth) {
+  if (nth < 0) return 'OOPS';
 
+  let prev = 1;
+  let curr = 0; 
+  
+  for (let i = 0; i < nth; i++) {
+    let next = prev + curr;
+    prev = curr;
+    curr = next;
+  }
+
+  return curr;
 };
 
 // Do not edit below this line

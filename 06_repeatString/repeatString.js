da8c978671f96ac8099f9d58b2eb2e0609c185e6
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2025-09-17 12:27:17 -0300

    Add repeatString function

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(text, times) {
+  if (times < 0) {
+    return "ERROR";
+  }
 
+  let textRepeated = "";
+  for (let i = 0; i < times; i++) {
+    textRepeated = textRepeated.concat(text);
+  }
+
+  return textRepeated;
 };
 
 // Do not edit below this line

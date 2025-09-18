8ce9ebfa1fb8c5b5ba758bd115c31c165f02a1ae
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2025-09-18 13:49:43 -0300

    Improved the code readbility

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,9 @@
 const leapYears = function(year) {
-  return year % 4 === 0  && year % 100 !== 0 || year % 400 === 0 
+  const isDivisibleBy4 = year % 4 === 0;
+  const isACentury = year % 100 === 0
+  const isDivisibleBy400 = year % 400 === 0;
+
+  return isDivisibleBy4 && !isACentury || isDivisibleBy400; 
 };
 
 // Do not edit below this line

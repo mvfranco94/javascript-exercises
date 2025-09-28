f8ac3bca46e9ec7e100eabd1c2ed8c353daff4df
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2025-09-28 15:15:48 -0300

    Add implementation of required functions

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(integers) {
+  return integers.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(integers) {
+  return integers.reduce((product, num) => product * num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let result = 1;
+  for	(let i = 0; i < exponent; i++) {
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n == 1 || n == 0) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
